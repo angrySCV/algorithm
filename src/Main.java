c22dd7453dd15c94aa881323926c391aff8fbe84
Dima Kubitskiy <dima@bepromo.ru>
2014-12-06 04:23:36 +0300

    добавка из теории чисел

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
-
-import static algorithm.numberTheory.phi;
+import static algorithm.numberTheory.nod;
+import static algorithm.numberTheory.nok;
 
 public class Main {
 
 	public static void main (String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int chislo = scanner.nextInt();
-		int res = phi(chislo);
-		System.out.println(res);
+		//Scanner scanner = new Scanner(System.in);
+//		int chislo = scanner.nextInt();
+//		int res = phi(chislo);
+		System.out.println(nod(25, 40) + " nok: " + nok(25, 40));
 	}
 }
--- src/algorithm/numberTheory.java
+++ src/algorithm/numberTheory.java
@@ -10,7 +10,7 @@ public final class numberTheory {
 	}
 
 	public static int phi (int chislo) {
-		//Eller function
+		//Eller function - вычисление функции Eллера
 		result = chislo;
 		for (int i = 2; i * i <= chislo; ++i)
 			if (chislo % i == 0) {
@@ -21,5 +21,26 @@ public final class numberTheory {
 		if (chislo > 1)
 			result = result - (result / chislo);
 		return result;
+	}
+
+	public static int nod (int a, int b) {
+		// Алгоритм Евклида - для нахождения наибольшего общего делителя
+		int temp;
+		if (b > a) {
+			temp = a;
+			a = b;
+			b = temp;
+		}
+		while (b != 0) {
+			temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+
+	public static int nok (int a, int b) {
+		// наименьшее общее кратное - используя наибольший общий делитель
+		return a * b / nod(a, b);
 	}
 }
